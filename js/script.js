0e084fe5ff70f3a11446c21e01805135b45dd967
Claire-Bedrossian <Claire-Bedrossian@users.noreply.github.com>
2022-12-02 19:16:21 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -19,16 +19,40 @@ if (navigator.serviceWorker) {
  * This function displays an alert.
  */
 function myButtonClicked() {
-  	var coffeeSizePrice = parseFloat(document.getElementById('coffee-size').value)
-  	var extrasPrice = parseFloat(document.getElementById('extras').value)
-    var finalPriceDiscount = coffeeSizePrice + extrasPrice + (0.13 * coffeeSizePrice + extrasPrice) - (0.25 * coffeeSizePrice + extrasPrice)
-    var finalPrice = coffeeSizePrice + extrasPrice + (0.13 * coffeeSizePrice + extrasPrice)
-    var subtotal = coffeeSizePrice + extrasPrice
-    var subtotalDiscount = coffeeSizePrice + extrasPrice - (0.25 * coffeeSizePrice + extrasPrice)
-  
-  if (coffeeSizePrice + extrasPrice >= 3.50) {
-    document.getElementById('total-price').innerHTML = `Coffee Type: $ ${coffeeSizePrice.toFixed(2)}</br>Extras: $ ${extrasPrice.toFixed(2)}</br>Your subtotal is: $ ${subtotal.toFixed(2)}</br>HST: 13% </br>Discount: 25% </br>Your final cost will be: $ ${finalPriceDiscount.toFixed(2)}`
+  var coffeeSizePrice = parseFloat(document.getElementById("coffee-size").value)
+  var extrasPrice = parseFloat(document.getElementById("extras").value)
+  var finalPriceDiscount =
+    coffeeSizePrice +
+    extrasPrice +
+    (0.13 * coffeeSizePrice + extrasPrice) -
+    (0.25 * coffeeSizePrice + extrasPrice)
+  var finalPrice =
+    coffeeSizePrice + extrasPrice + (0.13 * coffeeSizePrice + extrasPrice)
+  var subtotal = coffeeSizePrice + extrasPrice
+  var subtotalDiscount =
+    coffeeSizePrice + extrasPrice - (0.25 * coffeeSizePrice + extrasPrice)
+
+  if (coffeeSizePrice + extrasPrice >= 3.5) {
+    document.getElementById(
+      "total-price"
+    ).innerHTML = `Coffee Type: $ ${coffeeSizePrice.toFixed(
+      2
+    )}</br>Extras: $ ${extrasPrice.toFixed(
+      2
+    )}</br>Your subtotal is: $ ${subtotal.toFixed(
+      2
+    )}</br>HST: 13% </br>Discount: 25% </br>Your final cost will be: $ ${finalPriceDiscount.toFixed(
+      2
+    )}`
   } else {
-    document.getElementById('total-price').innerHTML = `Coffee Type: $ ${coffeeSizePrice.toFixed(2)}</br>Extras: $ ${extrasPrice.toFixed(2)}</br>Your subtotal is: $ ${subtotal.toFixed(2)}</br>HST: 13% </br>Your final cost will be: $ ${finalPrice.toFixed(2)}`
+    document.getElementById(
+      "total-price"
+    ).innerHTML = `Coffee Type: $ ${coffeeSizePrice.toFixed(
+      2
+    )}</br>Extras: $ ${extrasPrice.toFixed(
+      2
+    )}</br>Your subtotal is: $ ${subtotal.toFixed(
+      2
+    )}</br>HST: 13% </br>Your final cost will be: $ ${finalPrice.toFixed(2)}`
   }
 }
